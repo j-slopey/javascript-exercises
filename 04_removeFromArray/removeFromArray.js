9c7aa2186a6991efef1471d942d08d76ecced03b
James Slopey <181002700+j-slopey@users.noreply.github.com>
2024-12-11 14:35:04 -0600

    Completed excercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const args = [...arguments];
+    values = args.slice(1);
+    let out = array.filter((element) => values.indexOf(element) === -1);
+    return out;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
