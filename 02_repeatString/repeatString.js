469f4b210309511e56a2fc4d0d6d77f8ec869c12
James Slopey <181002700+j-slopey@users.noreply.github.com>
2024-12-11 13:14:30 -0600

    finished excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
 
+
+const repeatString = function(string1, count) {
+    let output = "";
+    if(count < 0){
+        return "ERROR"
+    }
+    for(let i = 0; i < count; i++){
+        output += string1;
+    }
+    return output;
 };
 
 // Do not edit below this line
