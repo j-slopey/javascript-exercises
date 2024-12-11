035c6e06c1a915acd8da780bef17199466646398
James Slopey <181002700+j-slopey@users.noreply.github.com>
2024-12-11 14:55:47 -0600

    Completed Excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let start, end;
+    if(num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    } else if(num2 > num1){
+        start = num1;
+        end = num2;
+    } else {
+        start = num2;
+        end = num1;
+    }
+    let sum = 0;
+    for(let i = start; i <= end; i++){
+       sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
