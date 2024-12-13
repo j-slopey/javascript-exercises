30f743da4be2466a5c60acf9006450827133b242
James Slopey <181002700+j-slopey@users.noreply.github.com>
2024-12-13 12:23:05 -0600

    Completed Activity 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce( (total, num) => total += num , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce( (total, num) => total *= num , 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  while(num > 0){
+      total *= num;
+      num -=1;
+    }
+  return total;
 };
 
 // Do not edit below this line
