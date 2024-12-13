e0336f84a0529b5613f253b4eb4685923beccd4c
James Slopey <181002700+j-slopey@users.noreply.github.com>
2024-12-13 13:01:46 -0600

    Completed Activity 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+
+const palindromes = function (word) {
+    let noPunctLower = word.replace(/[ !"#$%&'()*+,-./:;<=>?@^_`{|}~]/g, '').toLowerCase();
+    console.log(noPunctLower)
+    return ( noPunctLower === noPunctLower.split('').reverse().join('') )
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
