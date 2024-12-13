a77caa5042b48418c3f368139e22a07042632358
James Slopey <181002700+j-slopey@users.noreply.github.com>
2024-12-13 14:21:17 -0600

    Completed Activity 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    let oldestAge, personAge;
+    people.forEach( (person) => {
+        if(person.yearOfDeath === undefined){
+            person.yearOfDeath = 2024;
+        }
+        oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        personAge = person.yearOfDeath - person.yearOfBirth;
+        if(personAge > oldestAge){
+            oldest = person;
+        }
+    })
+    return oldest;
 };
 
 // Do not edit below this line
