cdcfe07c8fa42585852a1480f75573a80ecce341
James Slopey <181002700+j-slopey@users.noreply.github.com>
2024-12-13 13:11:53 -0600

    Completed Activity 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if( num == 0){
+        return 0;
+    } else if(num < 0){
+        return "OOPS"
+    }
+    let fib = [1,1];
+    for (let i = 1; i < num - 1; i++){
+        fib.push( fib[i] + fib[i-1])
+    }
+    return fib[fib.length - 1];
 };
 
 // Do not edit below this line
